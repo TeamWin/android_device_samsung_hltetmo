747908e5c8037b78ac7a4a4b21e5fb5db43de396
bigbiff <bigbiff@teamw.in>
2021-11-27 17:56:19 +0000

    hltetmo: remove goldfish dependency

--- omni_hltetmo.mk
+++ omni_hltetmo.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := hltetmo
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
